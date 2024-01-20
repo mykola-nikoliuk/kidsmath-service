1aeaf5ce02c5d47dd2dbeb684f39b5f2d8c0e3db
Mykola Nikoliuk <mykola.nikoliuk@gmail.com>
2024-01-20 13:29:16 -0700

    use body instead of query params

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import bodyParser from 'body-parser';
 import { createAppState } from '../models/AppState';
 import { User } from '../models/User';
 
@@ -6,6 +7,9 @@ export const usersRouter = Router();
 export const userRouter = Router();
 
 const state = createAppState();
+
+usersRouter.use(bodyParser.urlencoded());
+usersRouter.use(bodyParser.json());
 
 usersRouter.use('/:id', (req, res, next) => {
   const { id } = req.params;
@@ -31,7 +35,7 @@ userRouter.get('/dashboard', (req, res) => {
 
 userRouter.post('/tryAnswer', (req, res) => {
   const user: User = res.locals.user;
-  const answer = req.query.answer as string;
+  const answer = req.body.answer as string;
   res.json({
     dashboard: user.tryAnswer(answer),
     answers: user.exercises.getAvailableAnswers(),
